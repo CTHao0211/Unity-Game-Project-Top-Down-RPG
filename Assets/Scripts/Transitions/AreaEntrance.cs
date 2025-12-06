213f386206d3d65f43e6953fa0ba9af9df8e5924
Dat tran <tranhuynhsydat@gmail.com>
2025-12-06 19:16:42 +0700

    thêm chuyển cảnh

---
+++ Assets/Scripts/Transitions/AreaEntrance.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AreaEntrance : MonoBehaviour
+{
+    [SerializeField] private string transitionName;
+
+    private void Start()
+    {
+        // Không có SceneManagement thì thôi, khỏi làm gì
+        if (SceneManagement.Instance == null)
+        {
+            Debug.LogWarning("AreaEntrance: SceneManagement.Instance is null");
+            return;
+        }
+
+        if (transitionName == SceneManagement.Instance.SceneTransitionName)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                player.transform.position = transform.position;
+            }
+        }
+    }
+}
